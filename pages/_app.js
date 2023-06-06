9a230ac2b250ea301da6d589eee6a5e5c6f3e2e3
Bryan Cox <59836856+holypwnage316@users.noreply.github.com>
2023-06-06 17:50:08 -0400

    Update Bootstrap

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,12 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import '@styles/globals.css'
 
-function Application({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function Application({Component, pageProps}) {
+    useEffect(() => {
+        import ("bootstrap/dist/js/bootstrap");
+    }, []);
+
+    return <Component {...pageProps} />
 }
 
 export default Application
